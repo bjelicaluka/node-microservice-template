ed5a719a6262455c839a335d245365809896a77f
Luka Bjelica <bjelicaluka99@gmail.com>
2020-12-18 10:38:10 +0100

    feat: implemented simple top-level error handler

---
+++ src/exception/ExceptionHandler.ts
@@ -0,0 +1,39 @@
+import { Response } from 'express';
+const LOGGING = true;
+
+interface ErrorResponse {
+  statusCode: number;
+  message: string;
+}
+
+const ErrorNameCode = {
+  QueryFailedError: 400,
+}
+
+// Singleton
+export class ErrorHandler {
+  private static instance: ErrorHandler;
+
+  private constructor() { }
+
+  static getInstance(): ErrorHandler {
+    if (!ErrorHandler.instance) {
+      ErrorHandler.instance = new ErrorHandler();
+    }
+    return ErrorHandler.instance;
+  }
+
+  public handleError(e: Error): ErrorResponse {
+    LOGGING && console.log(e.name);
+    return {
+      statusCode: ErrorNameCode[e.name] || 500,
+      message: e.message
+    }
+  }
+
+  public sendErrorResponse(errorResponse: ErrorResponse, res: Response) {
+    res.status(errorResponse.statusCode);
+    res.send({ message: errorResponse.message });
+  }
+
+}
--- src/installers/RoutesInstaller.ts
+++ src/installers/RoutesInstaller.ts
@@ -1,26 +1,35 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import { IInstaller } from "../contracts/IInstaller";
+import { ErrorHandler } from "../exception/ExceptionHandler";
 import { Routes } from "../routes";
 
 export class RoutesInstaller implements IInstaller {
-    private app: any;
+  private app: any;
 
-    constructor(app: any) {
-        this.app = app;
-    }
+  constructor(app: any) {
+    this.app = app;
+  }
 
-    install(): void {
-        Routes.forEach(route => {
-            (this.app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-                const result = (new (route.controller as any))[route.action](req, res, next);
-                if (result instanceof Promise) {
-                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-    
-                } else if (result !== null && result !== undefined) {
-                    res.json(result);
-                }
+  install(): void {
+    Routes.forEach(route => {
+      (this.app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+        const result = (new (route.controller as any))[route.action](req, res, next);
+        if (result instanceof Promise) {
+          result
+            .then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+            .catch((error: Error) => {
+              const errorHandler = ErrorHandler.getInstance();
+              const errorResponse = errorHandler.handleError(error);
+              errorHandler.sendErrorResponse(errorResponse, res);
             });
-        });
-    }
+        } else if (result !== null && result !== undefined) {
+          res.json(result);
+        } else {
+          res.status(500);
+          res.send();
+        }
+      });
+    });
+  }
 
 }
