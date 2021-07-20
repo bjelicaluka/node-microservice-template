cd004f1532606a4864bd25344088225a99ad072f
Luka Bjelica <bjelicaluka99@gmail.com>
2021-07-20 13:07:43 +0200

    feat: added custom middleware to middleware installer

--- src/installers/MiddlewareInstaller.ts
+++ src/installers/MiddlewareInstaller.ts
@@ -1,6 +1,8 @@
 import express, { Application } from "express";
 import { inject, injectable } from "inversify";
+import { Route, Routes } from "../routes";
 import { IInstaller } from "./contracts/IInstaller";
+import cors from 'cors';
 
 @injectable()
 export class MiddlewareInstaller implements IInstaller {
@@ -13,6 +15,17 @@ export class MiddlewareInstaller implements IInstaller {
     install(): void {
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(express.json());
+        this.app.use(cors({origin: '*'}));
+
+        Routes.forEach(routes => {
+            routes.routes.forEach(route => {
+                route.middleware?.forEach(this.useMiddleware(route))
+            });
+        });
+    }
+
+    private useMiddleware = (route: Route) => (middleware: Function) => {
+        this.app[route.method](route.route, middleware)
     }
 
 }
